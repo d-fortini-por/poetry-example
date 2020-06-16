67e3cfad1189bbea33b368b5d5c9dfeefd65d793
d-fortini-por <d.fortini@portofrotterdam.com>
2020-06-16 08:26:32 +0200

    fixed poetry error

--- shell_scripts/install_poetry.sh
+++ shell_scripts/install_poetry.sh
@@ -1,4 +1,4 @@
-if poetry --version; then
+if poetry --version >/dev/null; then
     echo "poetry installed"
 else
     curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python
